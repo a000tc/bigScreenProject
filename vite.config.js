3c3e3f797f79d59db38bc8d7b010ef5c4f90180f
yangshengzhen <a000tc@163.com>
2024-03-06 11:46:47 +0800

    update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,14 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/bigScreenProject/",
   plugins: [
     vue(),
   ],
-  base: "/bigScreenProject/",
   // 打包配置
   build: {
     // target: 'modules',
-    // publicPath: '/bigScreenProject/',
+    // publicPath: './',
     // outDir: 'dist', //指定输出路径
     // assetsDir: 'assets', // 指定生成静态资源的存放路径
   },
