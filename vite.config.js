f79519c6035abf05f60afbad51aab643951e6575
yangshengzhen <a000tc@163.com>
2024-03-06 13:40:00 +0800

    update outDir

--- vite.config.js
+++ vite.config.js
@@ -12,7 +12,7 @@ export default defineConfig({
   build: {
     // target: 'modules',
     // publicPath: './',
-    // outDir: 'dist', //指定输出路径
+    outDir: 'docs', //指定输出路径
     // assetsDir: 'assets', // 指定生成静态资源的存放路径
   },
   resolve: {
