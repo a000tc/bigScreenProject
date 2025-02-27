400150f2ca85dc9cdfcafbc21d0b239ee14f474d
yangshengzhen <a000tc@163.com>
2025-02-27 23:54:08 +0800

    Update index.js

--- src/http/config/index.js
+++ src/http/config/index.js
@@ -2,4 +2,4 @@ const serverConfig = {
     baseURL: "https://apifoxmock.com/m1/4054441-0-default", // 请求基础地址,可根据环境自定义
     useTokenAuthorization: false, // 是否开启 token 认证
   };
-  export default serverConfig;  
+  export default serverConfig;
